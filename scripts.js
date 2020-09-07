a5b44ab0bf9017dfb816cb6cd58d9a117c5a77cc
Stephen Amaya <57957346+stephenamaya@users.noreply.github.com>
2020-09-07 17:31:02 -0400

    more fun functions

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
     
-    
-// function for lanuchpad to earth 
+  // function for lanuchpad to earth, and nav to launchpad
 
     // Document ready shorthand statement
     $(function() {
@@ -14,6 +13,16 @@
           );
   
           // Prevent default behavior of link
+          return false;
+        });
+      });
+
+      $(function() {
+        $('#launch-btn').click(function() {
+          $('html,body').animate({
+            scrollTop: $("#launch").offset().top }, 
+            7000 
+          );
           return false;
         });
       });
@@ -110,12 +119,74 @@
         });
       });
 
-      $(function() {
-        $('#launch-btn').click(function() {
-          $('html,body').animate({
-            scrollTop: $("#launch").offset().top }, 
-            7000 
-          );
-          return false;
-        });
-      });
+// earth population 
+
+/*
+* World Populution Counter - By JavaScript Kit (http://www.javascriptkit.com)
+* Based on code by Adam Brown
+* This notice MUST stay intact for use
+* Visit JavaScript Kit (http://www.javascriptkit.com) for this script and more
+*/
+
+function worldpopulation(){
+	startdate = new Date()
+	now(startdate.getYear(),startdate.getMonth(),startdate.getDate(),startdate.getHours(),startdate.getMinutes(),startdate.getSeconds())
+}
+
+
+function ChangeValue(number,pv){
+	numberstring =""
+	var j=0 
+	var i=0
+	while (number > 1)
+	 { 
+	    numberstring = (Math.round(number-0.5) % 10) + numberstring
+	    number= number / 10
+	    j++
+	    if (number > 1 && j==3) { 
+			numberstring = "," + numberstring 
+			j=0}
+	    i++
+	 }
+
+	 numberstring=numberstring
+
+if (pv==1) {document.getElementById("worldpop").innerHTML=numberstring }
+}
+
+function now(year,month,date,hours,minutes,seconds){       
+startdatum = new Date(year,month,date,hours,minutes,seconds)
+
+var now = 5600000000.0
+var now2 = 5690000000.0
+var groeipercentage = (now2 - now) / now *100
+var groeiperseconde = (now * (groeipercentage/100))/365.0/24.0/60.0/60.0 
+nu = new Date ()                
+schuldstartdatum = new Date (96,1,1)                            
+secondenoppagina = (nu.getTime() - startdatum.getTime())/1000
+totaleschuld= (nu.getTime() - schuldstartdatum.getTime())/1000*groeiperseconde + now
+ChangeValue(totaleschuld,1);
+
+
+timerID = setTimeout("now(startdatum.getYear(),startdatum.getMonth(),startdatum.getDate(),startdatum.getHours(),startdatum.getMinutes(),startdatum.getSeconds())",200)
+}
+
+
+// day of the week for the landing page
+
+function dayofweek() {
+  var d = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var n = weekday[d.getDay()];
+  document.getElementById("dayoweek").innerHTML = n;
+}
+
+// show nav after scroll
